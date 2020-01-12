3203cbf4057f533f280709b5332bf526920a2b73
Piotr Marczewski <piotrmarczewski133@gmail.com>
2020-01-12 21:31:34 +0100

    Dodanie klasy do obsługi bazy danych

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import sqlite3
 
-conn = sqlite3.connect('sejm_gov_pl_db.db')
+from Database import Database
+
+conn = sqlite3.connect('src/sejm_gov_pl_db.db', detect_types=sqlite3.PARSE_COLNAMES)
 print(conn)
 
-c = conn.cursor()
+db = Database('src/sejm_gov_pl_db.db')
+for row in db.query('SELECT last_party, COUNT(*) FROM speech_data sd JOIN portraits po ON sd.id_=po.id_ GROUP BY last_party ORDER BY COUNT(*) desc'):
+    print(row)
 
-t = ('%Adam%',)
-c.execute('SELECT * FROM portraits WHERE full_name LIKE ?', t)
-print(c.fetchone())
+# c = conn.cursor()
+# c2 = conn.cursor()
+#
+# # t = ('%Adam Abramowicz%',)
+# # for row in c.execute('SELECT party_section FROM portraits'):
+# #     print(row)
+# #     a = (row[1],)
+# #     for row2 in c2.execute('SELECT count(*) FROM speech_data WHERE id_=?', a):
+# #         print(row2)
+#
+#
+# t = ('%polskie%',)
+# for row in c.execute('SELECT last_party, COUNT(*) FROM speech_data sd JOIN portraits po ON sd.id_=po.id_ GROUP BY last_party ORDER BY COUNT(*) desc'):
+#     print(row)
